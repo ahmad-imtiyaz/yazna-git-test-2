4683d10bcdf36b2fbd73e284a50a51ab4397d169
ahmad-imtiyaz <imtiyaznajih8@gmail.com>
2025-03-30 22:08:41 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>wekxsnhjcwdbhcwsdhcbhs</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi illo rem porro cumque eos dolorum quis nesciunt a facilis numquam ullam in tempore perspiciatis pariatur corporis, eius quasi culpa saepe?</p>
+    <ul>
+        <li>satu</li>
+        <li>satu dua</li>
+    </ul>
 </body>
 
 </html>
